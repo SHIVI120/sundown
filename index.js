dfc8c45c38bde08febba08541e33242d9ac3b2d3
SHIVI120 <135041525+SHIVI120@users.noreply.github.com>
2024-07-12 08:42:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+var a=document.querySelector("#elem1")
+var image = a.getAttribute("data-image")
+console.log(image)
+
+
+   // var elemC = document.querySelector("#elem-container")
+    //var fixed = document.querySelector("#fixed-image")
+    //elemC.addEventListener("mouseenter", function () {
+      //  fixed.style.display = "block"
+    //})
+   
